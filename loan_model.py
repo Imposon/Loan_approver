80631cbfe38bde5759d60ac70c2b7165400b9a69
Imposon <8461000993as@gmail.com>
2026-01-30 23:22:55 +0530

    updated code

--- loan_model.py
+++ loan_model.py
@@ -5,3 +5,21 @@ data = pd.read_csv("data/loan_data.csv")
 print("Dataset loaded successfully!")
 print(data.head())
 print("Shape:", data.shape)
+
+print("\nMissing values before cleaning:\n", data.isnull().sum())
+
+cat_cols = [
+    'Gender', 'Married', 'Dependents',
+    'Self_Employed', 'Property_Area'
+]
+
+for col in cat_cols:
+    data[col].fillna(data[col].mode()[0], inplace=True)
+
+data['LoanAmount'].fillna(data['LoanAmount'].median(), inplace=True)
+data['Loan_Amount_Term'].fillna(data['Loan_Amount_Term'].mode()[0], inplace=True)
+data['Credit_History'].fillna(data['Credit_History'].mode()[0], inplace=True)
+
+print("\nMissing values after cleaning:\n", data.isnull().sum())
+print("Shape after cleaning:", data.shape)
+
