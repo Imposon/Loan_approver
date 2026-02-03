33888f244f32068b73da8c7a1d74a44a3c97a7b4
Imposon <8461000993as@gmail.com>
2026-02-03 21:28:18 +0530

    Add data preprocessing and logistic regression

--- loan_model.py
+++ loan_model.py
@@ -1,4 +1,11 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 
 data = pd.read_csv("data/loan_data.csv")
 
@@ -23,3 +30,28 @@ data['Credit_History'].fillna(data['Credit_History'].mode()[0], inplace=True)
 print("\nMissing values after cleaning:\n", data.isnull().sum())
 print("Shape after cleaning:", data.shape)
 
+
+data_encoded = pd.get_dummies(data, drop_first=True)
+X = data_encoded.drop('Loan_Status_Y', axis=1)
+y = data_encoded['Loan_Status_Y']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+
+print("\nConfusion Matrix:\n", cm)
+
+plt.figure(figsize=(6, 4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Loan Approval Confusion Matrix")
+plt.show()
+
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Model Accuracy:", accuracy)
